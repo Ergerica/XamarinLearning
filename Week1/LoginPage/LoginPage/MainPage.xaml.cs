f4fb27b5aae93111c0b2e4e74f451d5159a6bc14
Erica Ross <ericaross@Ericas-MacBook-Pro.local>
2019-08-16 02:26:17 -0400

    updloading first

---
+++ Week1/LoginPage/LoginPage/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LoginPage
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, EventArgs e)
+        {
+            if (this.password.Text == null && this.user.Text == null)
+            {
+                DisplayAlert("Error", "No username or password inserted", "OK");
+            }
+            else if (this.user.Text == null)
+            {
+                DisplayAlert("Error", "No username inserted", "OK");
+            }
+            else if (this.password.Text == null)
+            {
+                DisplayAlert("Error", "No password inserted", "OK");
+            }
+            else
+            {
+                DisplayAlert("Bien", $"Welcome {this.user.Text}, to the fantastic worl of Xamarin!", "OK");
+            }
+
+        }
+
+    }
+}
